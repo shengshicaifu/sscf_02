8ec892938d5c96bf21c44c81f57c9e780610ff19
shengshicaifu <734198511@qq.com>
2015-03-24 13:54:28 +0800

    蒋飞

--- 盛世财富/盛世财富/RealNameViewController.swift
+++ 盛世财富/盛世财富/RealNameViewController.swift
@@ -10,14 +10,41 @@ import Foundation
 import UIKit
 
 //实名认证
-class RealNameViewController: UIViewController {
+class RealNameViewController: UIViewController,UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
+        realNameText.delegate = self
+        idNumberText.delegate = self
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    @IBOutlet weak var realNameText: UITextField!
+    @IBOutlet weak var idNumberText: UITextField!
     @IBAction func returnTapped(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
+    @IBAction func commitTapped(sender: AnyObject) {
+     var alert = UIAlertController(title: "提示", message: "你输入的用户名和身份证号不能为空", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "确定", style: UIAlertActionStyle.Cancel, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
+      //隐藏键盘
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        realNameText.resignFirstResponder()
+        idNumberText.resignFirstResponder()
+    }
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        realNameText.resignFirstResponder()
+        idNumberText.resignFirstResponder()
+        return true
+    }
+//     func textFieldShouldBeginEditing(textField: UITextField) -> Bool{
+//        
+//        return true
+//    }
+
+
 }

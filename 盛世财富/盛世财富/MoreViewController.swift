97562abe8dfc87ee156f46e4fd40c0cd6ac89f12
YunJianx <601390610@qq.com>
2015-03-24 12:00:24 +0800

    add

---
+++ 盛世财富/盛世财富/MoreViewController.swift
@@ -0,0 +1,53 @@
+//
+//  MoreViewController.swift
+//  盛世财富
+//
+//  Created by xiao on 15-3-24.
+//  Copyright (c) 2015年 sscf88. All rights reserved.
+//
+
+
+
+import UIKit
+
+class MoreViewController: UIViewController ,UITableViewDataSource,UITableViewDelegate{
+
+    @IBOutlet weak var mainTable: UITableView!
+    
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+        mainTable.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell!
+        
+        switch indexPath.row {
+        case 0:
+            cell = self.mainTable.dequeueReusableCellWithIdentifier("forum") as UITableViewCell
+            
+        case 1:
+            cell = self.mainTable.dequeueReusableCellWithIdentifier("update") as UITableViewCell
+            
+        case 2:
+            cell = self.mainTable.dequeueReusableCellWithIdentifier("feedback") as UITableViewCell
+        case 3:
+            cell = self.mainTable.dequeueReusableCellWithIdentifier("about") as UITableViewCell
+        default: cell = nil
+        }
+        return cell
+    }
+
+}
+

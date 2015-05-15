2bbd87406389bc1813fe38277831bde4d119b228
云笺 <yunjian@yunjiandeMac-4.local>
2015-05-15 13:31:44 +0800

    M

--- 盛世财富/盛世财富/RegisterViewController.swift
+++ 盛世财富/盛世财富/RegisterViewController.swift
@@ -1,7 +1,7 @@
 //
 //  TransRecordViewController.swift
 //  盛世财富
-//
+//  注册
 //  Created by zengchang on 15-3-17.
 //  Copyright (c) 2015年 sscf88. All rights reserved.
 //
@@ -12,11 +12,13 @@ import UIKit
 class RegisterViewController: UIViewController,UITextFieldDelegate {
     
     
+    @IBOutlet weak var codeTextField: UITextField!
     @IBOutlet weak var surePwdTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var phoneTextField: UITextField!
     @IBOutlet weak var regist: UIButton!
     @IBOutlet weak var checkBtn: UIButton!
+    var timer:NSTimer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,30 +31,115 @@ class RegisterViewController: UIViewController,UITextFieldDelegate {
         self.navigationItem.title = "用户注册"
     }
     
+    //发送验证码
     @IBAction func checkTapped(sender: UIButton) {
-        //验证码
+        var phone = phoneTextField.text
+        
+        if phone.isEmpty {
+            AlertView.showMsg("手机号码不能为空", parentView: self.view)
+            return
+        }
+        
+        //网络等待指示器
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        
+        //禁用获取验证码按钮60秒
+        checkBtn.enabled = false
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "repeat", userInfo: nil, repeats: true)
+        
+        //获取验证码
+        var url = "http://www.sscf88.com/App-Register-sendphone"
+        var params = ["cellphone":phone]
+        var manager = AFHTTPRequestOperationManager()
+        manager.POST(url, parameters: params,
+            success: { (op:AFHTTPRequestOperation!, data:AnyObject!) -> Void in
+                UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+                var result = data as! NSDictionary
+                NSLog("验证码%@", result)
+                var code = result["code"] as! Int
+                var msg:String = ""
+                if code == 0 {
+                    msg = "验证码生送失败，请重试!"
+                }else if code == 1 {
+                    msg = "手机号已被别人使用!"
+                }else if code == 100 {
+                    msg = "短信验证码发送成功"
+                }
+                AlertView.showMsg(msg, parentView: self.view)
+            },
+            failure:{ (op:AFHTTPRequestOperation!, error:NSError!) -> Void in
+            }
+        )
     }
+    
+    var i = 60
+    func repeat(){
+        checkBtn.setTitle("重新获取（\(i)）", forState: UIControlState.Disabled)
+        i--
+        if i == 0 {
+            i = 60
+            timer.invalidate()
+            checkBtn.enabled = true
+            checkBtn.setTitle("发送验证码", forState: UIControlState.Normal)
+        }
+    }
+    
+    //注册
     @IBAction func registerTapped(sender: AnyObject) {
-        loading.startLoading(self.view)
+
+//        self.performSegueWithIdentifier("registerToPersonCenterSegue", sender: nil)
+        self.navigationController?.popToViewController(NewPersonCenterViewController(), animated: true)
+        NSLog("跳转测试失败")
+        
+        
         surePwdTextField.resignFirstResponder()
         passwordTextField.resignFirstResponder()
         phoneTextField.resignFirstResponder()
-        
+        codeTextField.resignFirstResponder()
         var surePwd = surePwdTextField.text
         var password = passwordTextField.text
         var phone = phoneTextField.text
+        var code = codeTextField.text
         if phone.isEmpty {
             AlertView.showMsg("手机号码不能为空", parentView: self.view)
         }else if password.isEmpty {
             AlertView.showMsg("密码不能为空", parentView: self.view)
         }else if password != surePwd{
             AlertView.showMsg("两次输入的密码不一致", parentView: self.view)
+        }else if code.isEmpty{
+            AlertView.showMsg("验证码不能为空", parentView: self.view)
         }else{
             //此处执行注册操作
 //            showAlert("执行注册操作")
 //            self.navigationController?.pushViewController(LendViewController(), animated: true)
+            loading.startLoading(self.view)
+            var url = "http://www.sscf88.com/App-Register-regaction"
+            var params = ["user_name":phone,"pass_word":password,"code":code]
+            var manager = AFHTTPRequestOperationManager()
+            manager.POST(url, parameters: params,
+                success: { (op:AFHTTPRequestOperation!, data:AnyObject!) -> Void in
+                    loading.stopLoading()
+                    var result = data as! NSDictionary
+                    NSLog("注册返回结果：%@",result)
+                    var code = result["code"] as! Int
+                    if code == 0 {
+                        AlertView.showMsg("手机号码不合法", parentView: self.view)
+                    } else if code == 1 {
+                        AlertView.showMsg("手机号码已经被注册", parentView: self.view)
+                    } else if code == 4 {
+                        AlertView.showMsg("手机校验码不正确", parentView: self.view)
+                    } else if code == 200 {
+                        //注册成功,保存基本信息，跳转到我的账号页面
+                        NSLog("注册成功")
+                    }
+                },
+                failure: { (op:AFHTTPRequestOperation!, error:NSError!) -> Void in
+                    NSLog("注册请求失败：%@", error)
+                    loading.stopLoading()
+                    AlertView.showMsg("注册失败", parentView: self.view)
+                }
+            )
         }
-        loading.stopLoading()
     }
     @IBAction func returnKey(sender:AnyObject){
         self.dismissViewControllerAnimated(true, completion: nil)
@@ -73,15 +160,11 @@ class RegisterViewController: UIViewController,UITextFieldDelegate {
     }
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
-        UIView.beginAnimations("ResizeForKeyboard", context: nil)
-        UIView.setAnimationDuration(0.3)
-        var rect:CGRect = CGRectMake( 0,  0,  self.view.frame.size.width,  self.view.frame.size.height)
-        self.view.frame = rect;
-        UIView.commitAnimations()
-        
+
         surePwdTextField.resignFirstResponder()
         passwordTextField.resignFirstResponder()
         phoneTextField.resignFirstResponder()
+        codeTextField.resignFirstResponder()
         return true
     }
     override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
@@ -89,5 +172,6 @@ class RegisterViewController: UIViewController,UITextFieldDelegate {
         surePwdTextField.resignFirstResponder()
         passwordTextField.resignFirstResponder()
         phoneTextField.resignFirstResponder()
+        codeTextField.resignFirstResponder()
     }
 }

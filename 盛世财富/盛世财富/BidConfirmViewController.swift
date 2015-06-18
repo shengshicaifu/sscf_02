b536d8e19d55d6003c9695e94cd4a63de2cf7da0
YunJianx <601390610@qq.com>
2015-06-18 11:11:40 +0800

    xd

--- 盛世财富/盛世财富/BidConfirmViewController.swift
+++ 盛世财富/盛世财富/BidConfirmViewController.swift
@@ -45,6 +45,7 @@ class BidConfirmViewController: UIViewController,UITextFieldDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
         resignAll()
+        
         // Do any additional setup after loading the view, typically from a nib.
         bidMoney.delegate = self
         reward.delegate = self
